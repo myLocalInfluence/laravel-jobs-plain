dd63cb31b9c022b2895b8d69e7e8c5a86d01bea2
Aurélien SCHILTZ <aure.lienschiltz@gmail.com>
2019-06-17 16:19:58 +0200

    feat: Implement SQS Driver
    Implement SQS Driver Push & Pull Raw Plain JSON jobs
    Closes #6

--- src/Jobs/DispatcherJob.php
+++ src/Jobs/DispatcherJob.php
@@ -17,11 +17,6 @@ class DispatcherJob implements ShouldQueue
     protected $data;
 
     /**
-     * @var bool
-     */
-    protected $plain = false;
-
-    /**
      * DispatchedJob constructor.
      *
      * @param $data
@@ -36,32 +31,6 @@ class DispatcherJob implements ShouldQueue
      */
     public function getPayload()
     {
-        if (!$this->isPlain()) {
-            return [
-                'job'  => app('config')->get('sqs-plain.default-handler'),
-                'data' => $this->data,
-            ];
-        }
-
         return $this->data;
-    }
-
-    /**
-     * @param bool $plain
-     * @return $this
-     */
-    public function setPlain($plain = true)
-    {
-        $this->plain = $plain;
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isPlain()
-    {
-        return $this->plain;
     }
 }
